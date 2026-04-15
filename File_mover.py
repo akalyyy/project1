940520fafa68112904f5ca5c8b0acd71180033a2
Aum Kalyan <aumkalyan183@gmail.com>
2026-04-15 22:58:45 +0100

    created a breif config json file with simple rules for folder naming

--- File_mover.py
+++ File_mover.py
@@ -1,8 +1,8 @@
 import os
 import shutil
 
-def move_file(filename, destination):
-    current_destination = os.path.join("input", filename)
+def move_file(filename, source, destination):
+    current_destination = os.path.join(source, filename)
     moved_destination = os.path.join(destination, filename)
 
     shutil.move(current_destination, moved_destination)
--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import os
+import json
 from Validating_names import validation
 from File_mover import move_file
 
-input_file = os.listdir("input")
-log_file = open("logs/log.txt", "w")
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)
+
+input_folder = os.listdir(config["input_folder"])
+log_file = open(f"{config['logs_folder']}/log.txt", "w")
 processed_count = 0
 quarantined_count = 0
 
-for file in input_file:
+for file in input_folder:
     if validation(file) == True:
-        move_file(file, "processed")
+        move_file(file, config["input_folder"], config["processed_folder"])
         log_file.write(f"{file} has been processed")
         processed_count+=1
     else:
-        move_file(file, "quarantine")
+        move_file(file, config["input_folder"], config["quarantine_folder"])
         log_file.write(f"{file} has been quarantined")
         quarantined_count+=1
 
@@ -21,9 +25,9 @@ log_file.close()
 
 print("\nSimple summary:")
 print("\nProcessed files:")
-print(os.listdir("processed"))
+print(os.listdir(config["processed_folder"]))
 print(f"Total processed files: {processed_count}")
 
 print("\nQuarantined files:")
-print(os.listdir("quarantine"))
+print(os.listdir(config["quarantine_folder"]))
 print(f"Total quarantined files: {quarantined_count}")
