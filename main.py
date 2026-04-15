9a75203d39d7eb8776242a3aa82dcd0d2a5e2b8b
Aum Kalyan <aumkalyan183@gmail.com>
2026-04-15 21:15:26 +0100

    creating a lof for what has been processed and what has been quarantined

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import os
 from Validating_names import validation
 from File_mover import move_file
 input_file = os.listdir("input")
+log_file = open("logs/log.txt", "w")
 
 for file in input_file:
     if validation(file) == True:
         move_file(file, "processed")
+        log_file.write("f{file} has been processed")
     else:
         move_file(file, "quarantine")
+        log_file.write(f"{file} has been quarantined")
 
+log_file.close()
 
 print("\nProcessed files:")
 print(os.listdir("processed"))
