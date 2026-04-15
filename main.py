55a44161c9a6223dbb05a2a34bd10d4c5f346f32
Aum Kalyan <aumkalyan183@gmail.com>
2026-04-15 21:30:34 +0100

    creating a simple numerical summary of processed and quarantined files

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import os
 from Validating_names import validation
 from File_mover import move_file
+
 input_file = os.listdir("input")
 log_file = open("logs/log.txt", "w")
+processed_count = 0
+quarantined_count = 0
 
 for file in input_file:
     if validation(file) == True:
         move_file(file, "processed")
-        log_file.write("f{file} has been processed")
+        log_file.write(f"{file} has been processed")
+        processed_count+=1
     else:
         move_file(file, "quarantine")
         log_file.write(f"{file} has been quarantined")
+        quarantined_count+=1
 
 log_file.close()
 
+print("\nSimple summary:")
 print("\nProcessed files:")
 print(os.listdir("processed"))
+print(f"Total processed files: {processed_count}")
 
 print("\nQuarantined files:")
 print(os.listdir("quarantine"))
+print(f"Total quarantined files: {quarantined_count}")
